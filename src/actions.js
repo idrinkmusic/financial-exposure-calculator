36d6ea7c61325dad6e77dd088bc473ed766cf6a2
Santiago Castro <idrinkmusicftw@gmail.com>
2018-04-09 03:42:49 +0200

    Refactor actions and reducers

--- src/actions.js
+++ src/actions.js
@@ -1,38 +1,54 @@
 export const REQUEST_PERSON_DETAILS = "REQUEST_PERSON_DETAILS";
 export const RECEIVE_PERSON_DETAILS = "RECEIVE_PERSON_DETAILS";
-export const GET_FACILITY_DETAILS = "GET_FACILITY_DETAILS";
-export const GET_EXPOSURE_DETAILS = "GET_EXPOSURE_DETAILS";
+export const REQUEST_FACILITY_DETAILS = "REQUEST_FACILITY_DETAILS";
+export const RECEIVE_FACILITY_DETAILS = "RECEIVE_FACILITY_DETAILS";
+export const REQUEST_EXPOSURE_DETAILS = "REQUEST_EXPOSURE_DETAILS";
+export const RECEIVE_EXPOSURE_DETAILS = "RECEIVE_EXPOSURE_DETAILS";
 
-export const requestPersonDetails = person => {
+const requestPersonDetails = personId => {
   return {
     type: REQUEST_PERSON_DETAILS,
-    person
+    personId
   };
 };
 
-export const receivePersonDetails = person => {
+const receivePersonDetails = person => {
   return {
     type: RECEIVE_PERSON_DETAILS,
     person
   };
 };
 
-const getFacilityDetails = facility => {
+const requestFacilityDetails = facility => {
   return {
-    type: GET_FACILITY_DETAILS,
+    type: REQUEST_FACILITY_DETAILS,
     facility
   };
 };
 
-const getExposureDetails = exposure => {
+const receiveFacilityDetails = facility => {
   return {
-    type: GET_EXPOSURE_DETAILS,
+    type: RECEIVE_FACILITY_DETAILS,
+    facility
+  };
+};
+
+const requestExposureDetails = exposure => {
+  return {
+    type: REQUEST_EXPOSURE_DETAILS,
+    exposure
+  };
+};
+
+const receiveExposureDetails = exposure => {
+  return {
+    type: RECEIVE_EXPOSURE_DETAILS,
     exposure
   };
 };
 
 const fetchPersonDetails = person => {
-  return fetch(`api/person/${person}`);
+  return fetch(`/api/person/${person}`);
 };
 
 const fetchFacilityDetails = facility => {
@@ -40,15 +56,33 @@ const fetchFacilityDetails = facility => {
 };
 
 const fetchExposureDetails = exposure => {
-  return fetch(`api/person/${exposure}`);
+  return fetch(`api/exposure/${exposure}`);
 };
 
-export const calculateExposure = person => {
+export const fetchDetails = person => {
   return dispatch => {
     dispatch(requestPersonDetails(person));
 
-    return fetchPersonDetails()
+    return fetchPersonDetails(person)
       .then(response => response.json())
-      .then(json => dispatch(getFacilityDetails(json.val1, json)));
+      .then(json => {
+        dispatch(receivePersonDetails(json));
+
+        dispatch(requestFacilityDetails(json.val1));
+
+        fetchFacilityDetails(json.val1)
+          .then(response => response.json())
+          .then(json2 => {
+            dispatch(receiveFacilityDetails(json2));
+
+            dispatch(requestExposureDetails(json.val2));
+
+            fetchExposureDetails(json.val2)
+              .then(response => response.json())
+              .then(json3 => {
+                dispatch(receiveExposureDetails(json3));
+              });
+          });
+      });
   };
 };
--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,45 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
-import { REQUEST_PERSON_DETAILS, RECEIVE_PERSON_DETAILS } from "./actions";
+import {
+  REQUEST_PERSON_DETAILS,
+  RECEIVE_PERSON_DETAILS,
+  REQUEST_FACILITY_DETAILS,
+  RECEIVE_FACILITY_DETAILS,
+  REQUEST_EXPOSURE_DETAILS,
+  RECEIVE_EXPOSURE_DETAILS
+} from "./actions";
 
-const selectedPersonId = (state = {}, action) => {
+const selectedPersonId = (
+  state = {
+    isFetching: false,
+    id: null,
+    personDetails: {},
+    facilityDetails: {},
+    exposureDetails: {}
+  },
+  action
+) => {
   switch (action.type) {
     case REQUEST_PERSON_DETAILS:
+      return Object.assign({}, state, {
+        id: action.personId,
+        isFetching: true
+      });
     case RECEIVE_PERSON_DETAILS:
-      return [...state, { personId: action.personId }];
+      return Object.assign({}, state, {
+        personDetails: action.person,
+        isFetching: true
+      });
+    case RECEIVE_FACILITY_DETAILS:
+      return Object.assign({}, state, {
+        facilityDetails: action.facility,
+        isFetching: true
+      });
+    case RECEIVE_EXPOSURE_DETAILS:
+      return Object.assign({}, state, {
+        exposureDetails: action.exposure,
+        isFetching: false
+      });
     default:
       return state;
   }
